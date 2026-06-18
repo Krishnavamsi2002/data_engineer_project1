2e7a07a64d8320a6137526381849408f9ecbac1c
Krishnavamsi2002 <pecchettikrishnavamsi@gmail.com>
2026-06-18 18:32:06 +0530

    new process_per_month_added

--- src/bronze/data_processing_bronze.py
+++ src/bronze/data_processing_bronze.py
@@ -13,13 +13,26 @@ def load_data():
        "D:\\projects\\data-engineering-project\\data\\raw\\retail_sales_dataset.csv",
         header=True,
         inferSchema=True
-
     )
+    data_frame.show(5)
     return data_frame
+
+def process_data_per_month(df):
+    df=df.withColumn("Month_name",F.date_format("Date", "MMMM")) 
+
+    df=df.groupBy("Month_name").agg(F.sum("Total Amount").alias("Total_Sales_per_Month"))
+    df.show(5)
+    return df
+
+def save_data(df):
+    output_path = Path("data/bronze/processed_data")
+    output_path.mkdir(parents=True, exist_ok=True)
+    df.write.csv(str(output_path), header=True, mode="overwrite")
 
 def main():
     df = load_data()
-    df.show(5)
+    processed_df = process_data_per_month(df)
+    #save_data(processed_df)
 
 if __name__ == "__main__":
     main()
